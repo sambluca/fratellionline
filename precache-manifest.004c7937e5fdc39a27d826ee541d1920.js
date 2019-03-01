9c336c8f64b8c39675a4235d664a2698b5ab580d
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2019-03-01 11:01:49 +0000

    Updates

--- precache-manifest.004c7937e5fdc39a27d826ee541d1920.js
+++ precache-manifest.004c7937e5fdc39a27d826ee541d1920.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fratellionline/static/js/runtime~main.21991be0.js"
   },
   {
-    "revision": "551cca58ad36c694e733",
-    "url": "/fratellionline/static/js/main.551cca58.chunk.js"
+    "revision": "dee90a21b654a1baebd5",
+    "url": "/fratellionline/static/js/main.dee90a21.chunk.js"
   },
   {
     "revision": "4d58323caa97fe5e9f7a",
     "url": "/fratellionline/static/js/2.4d58323c.chunk.js"
   },
   {
-    "revision": "551cca58ad36c694e733",
+    "revision": "dee90a21b654a1baebd5",
     "url": "/fratellionline/static/css/main.e13e3323.chunk.css"
   },
   {
-    "revision": "cbd2de229b064513bf88cc15ff89a9d7",
+    "revision": "522051ca19025b5109a596831cffe126",
     "url": "/fratellionline/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fratellionline/precache-manifest.86992096b10408398cff11ec6e5f8dbe.js"
+  "/fratellionline/precache-manifest.004c7937e5fdc39a27d826ee541d1920.js"
 );
 
 workbox.clientsClaim();
