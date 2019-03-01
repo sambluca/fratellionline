7b2c826391200bd677ead55916e474bc25209cb3
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2019-03-01 10:47:19 +0000

    Updates

---
+++ precache-manifest.86992096b10408398cff11ec6e5f8dbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21991be0d4b0438a9d98",
+    "url": "/fratellionline/static/js/runtime~main.21991be0.js"
+  },
+  {
+    "revision": "551cca58ad36c694e733",
+    "url": "/fratellionline/static/js/main.551cca58.chunk.js"
+  },
+  {
+    "revision": "4d58323caa97fe5e9f7a",
+    "url": "/fratellionline/static/js/2.4d58323c.chunk.js"
+  },
+  {
+    "revision": "551cca58ad36c694e733",
+    "url": "/fratellionline/static/css/main.e13e3323.chunk.css"
+  },
+  {
+    "revision": "cbd2de229b064513bf88cc15ff89a9d7",
+    "url": "/fratellionline/index.html"
+  }
+];
